19122d72f492a00857f49e23c2e0451f22d41264
Ender <33337309+e3ndr@users.noreply.github.com>
2020-12-04 19:44:28 -0600

    Add custom emotes

--- modules/rainmodule.js
+++ modules/rainmodule.js
@@ -31,10 +31,12 @@ MODULES.moduleClasses["casterlabs_rain"] = class {
         const instance = this;
 
         koi.addEventListener("chat", (event) => {
+            this.addCustomEmotes(event);
             MODULES.emitIO(instance, "event", event);
         });
 
         koi.addEventListener("donation", (event) => {
+            this.addCustomEmotes(event);
             MODULES.emitIO(instance, "event", event);
         });
     }
@@ -43,16 +45,35 @@ MODULES.moduleClasses["casterlabs_rain"] = class {
         MODULES.emitIO(this, "config", this.settings);
     }
 
+    addCustomEmotes(event) {
+        event.custom_emotes = {};
+
+        this.settings.custom_emotes.forEach((emote) => {
+            event.custom_emotes[emote.name] = emote.link;
+        })
+    }
+
     settingsDisplay = {
         "life_time (Seconds)": "number",
         max_emojis: "number",
-        size: "number"
+        size: "number",
+        custom_emotes: "dynamic"
     };
 
     defaultSettings = {
         "life_time (Seconds)": 10,
         max_emojis: 1000,
-        size: 20
+        size: 20,
+        custom_emotes: {
+            display: {
+                name: "input",
+                link: "input"
+            },
+            default: {
+                name: "Pog",
+                link: "https://cdn.frankerfacez.com/emoticon/210748/1"
+            }
+        }
     };
 
 };
